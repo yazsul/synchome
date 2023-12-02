31aa7199e384ef83869661243d1a3f6d1079a4c8
habel it <habeltechs@gmail.com>
2023-12-02 09:27:11 +0100

    route changed

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -12,7 +12,7 @@ const toggleDark = useToggle(isDark);
       <h1 class="text-4xl font-bold">Get Started with SyncH🥰me</h1>
       <p class="text-xl">Simplify Life with a Tap</p>
       <div class="flex flex-col items-center justify-center mt-8">
-        <button @click="router.push('/build-house')" class="px-4 py-2 bg-secondary text-white rounded-md hover:shadow-lg">Start Now</button>
+        <button @click="router.push('/address')" class="px-4 py-2 bg-secondary text-pp-blue hover:bg-slate-200 rounded-md hover:shadow-lg">Start Now</button>
       </div>
     </div>
   </div>
