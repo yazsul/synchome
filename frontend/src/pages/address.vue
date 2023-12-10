a66107839fa511f1fc2e5f3174875631c0bea192
tmk kassa <tmkkassa@gmail.com>
2023-12-10 20:26:34 +0100

    address component modified

--- frontend/src/pages/address.vue
+++ frontend/src/pages/address.vue
@@ -5,8 +5,8 @@ const toggleDark = useToggle(isDark);
 import useUser from "@/use/user";
 
 export default {
-  name: "address",
-  display: "Address",
+  name: "addresses",
+  display: "Addresses",
   order: 1,
   components: {},
   data() {
@@ -21,14 +21,18 @@ export default {
   methods: {
     emitMyEvent() {
       const user = useUser();
-      user.set({
+      let pvData = JSON.parse(window.localStorage.getItem("session"));
+      let fd = {
+        ...pvData,
         address: {
           streetNumber: parseInt(this.streetNumber),
           city: this.city,
           country: this.country,
           postcode: parseInt(this.postcode),
         },
-      });
+      }
+      console.log(fd);
+      user.set({...fd});
       this.$router.push("/build-house");
     },
   },
@@ -36,7 +40,7 @@ export default {
 };
 </script>
 <template>
-  <div class="h-screen">
+  <div class="h-screen text-slate-300">
     <div class="flex flex-col items-center justify-center h-full">
       <h1 class="text-4xl font-bold">Fill out this short form to get started!</h1>
       <div
@@ -44,7 +48,7 @@ export default {
       >
         <div class="w-3/4">
           <p class="text-xl text-center text-gray-300 mb-3">Address Info</p>
-          <HTextField v-model="streetNumber" name="street" rules="required">
+          <HTextField placeholderStyle="placeholder-gray-100" class="bg-white" v-model="streetNumber" name="street" rules="required">
             <template #label>
               <span class="text-gray-300 font-medium">Street No.</span></template
             >
@@ -67,7 +71,7 @@ export default {
           <div class="text-center w-full">
             <button
               @click="emitMyEvent"
-              class="px-4 py-2 bg-secondary text-pp-blue hover:bg-slate-200 rounded-md hover:shadow-lg w-3/4"
+              class="px-4 py-2 bg-pp-blue text-slate-300 border hover:bg-slate-200 hover:text-pp-blue rounded-md hover:shadow-lg w-3/4"
             >
               Next
             </button>
